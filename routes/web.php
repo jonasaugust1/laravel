460fa268ad403fb6785b513606bb38d070823e12
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-07-26 17:06:39 -0300

    Implementing Routes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,65 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('contato', function (){
+
+});
+
+Route::post('contato', function (){
+
+});
+
+Route::put('contato', function (){
+
+});
+
+Route::delete('contato', function (){
+
+});
+
+//Utilizar mais de um método http em uma rota
+Route::match(['get', 'post'], 'sobre', function(){
+
+});
+
+//Utilizar todos os métodos http
+Route::any('todos', function(){
+    //Gerar uma url
+    $url = url('nova');
+    return "Todos!" .$url;
+});
+
+//Receber parâmetros via rota
+Route::get('artigo/{id}', function ($id){
+    return "Artigo id: {$id}";
+});
+
+//Receber parâmetros opcionais via rota
+Route::get('produto/{id}', function ($id = null){
+    return "Artigo id: {$id}";
+});
+
+//Apelidos para rotas
+Route::get('teste', ['as' => 'detalhe', function (){
+    return "Teste";
+}]);
+
+//Links
+Route::get('link', ['as' => 'link', function (){
+    return 'Link <a href="'.route('detalhe').'">Detalhe</a>';
+}]);
+
+//Grupo de rotas
+Route::group(['prefix'=>'admin'], function(){
+    Route::get('contato', function (){
+        return 'Admin contato';
+    });
+
+    Route::get('sobre', function (){
+
+    });
 });
