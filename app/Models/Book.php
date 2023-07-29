259f298b00d01a0357cf73fd6e4fedb965e13cdc
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-07-29 09:41:40 -0300

    Using Tinker

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    //Para permitir registros em massa via Tinker - App\Models\Book::create(["title"=>"Novo Livro", "description"=>"desc novo livro"]);
+    protected $fillable = ['title', 'description'];
+    public function listaLivros(){
+        return $this->all();
+    }
 }
